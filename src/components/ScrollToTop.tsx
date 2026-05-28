bfa030bc5754078a351e30e8ad2980ea3d066d0d
Raven S <divinestarraven@gmail.com>
2026-05-29 06:14:56 +1000

    Start repository

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if ('scrollBehavior' in document.documentElement.style) {
+      window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
+
+  return null;
+}
